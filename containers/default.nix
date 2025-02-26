ea17fdf8315ab1441c0f1c6b7a03ff22404f5c8a
Cameron <cbadwork@gmail.com>
2025-02-26 21:36:23 +1000

    darwin system container files

--- containers/default.nix
+++ containers/default.nix
@@ -1,25 +1,40 @@
 { config, pkgs, lib, ... }:
 
-let utils = import ../utils.nix { inherit lib; };
-in {
-  imports = utils.getImports ./.;
-
-  # Enable Docker
-  virtualisation.docker.enable = true;
-
-  # Add Docker and related tools to system packages
-  environment.systemPackages = with pkgs; [
+let 
+  utils = import ../utils.nix { inherit lib; };
+  isDarwin = pkgs.stdenv.isDarwin;
+  isLinux = pkgs.stdenv.isLinux;
+  
+  # Common packages for both platforms
+  commonPackages = with pkgs; [
     docker
     docker-compose
     kubernetes-helm
     kubectl
     minikube
-    kubernetes-helm
     k9s
-    cloudlens
     vscode
   ];
-
-  # Add your user to the Docker group
-  users.users.cameron = { extraGroups = [ "docker" ]; };
+  
+  # Linux-specific packages
+  linuxPackages = with pkgs; [
+    cloudlens
+    # Add other Linux-only packages here
+  ];
+in {
+  imports = utils.getImports ./.;
+  
+  # Add packages to the system
+  environment.systemPackages = commonPackages 
+    ++ lib.optionals isLinux linuxPackages;
+  
+  # Linux-specific configurations
+  virtualisation = lib.mkIf isLinux {
+    docker.enable = true;
+  };
+  
+  # User configurations (Linux-only)
+  users.users = lib.mkIf (isLinux && config.users.users ? cameron) {
+    cameron.extraGroups = [ "docker" ];
+  };
 }
